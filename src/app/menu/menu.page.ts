b69619e8cbf721123084a8817e6e302b1f519b01
Oscar <rouquette.oscar@gmail.com>
2023-04-11 18:00:47 +0200

    css improvements + start media queries + search page finition

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -17,6 +17,24 @@ export class MenuPage implements OnInit {
     alias: ''
   }
 
+  breakpoints = {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  }
   constructor(private storage: Storage, private router: Router, private http: HttpClient) { }
 
   ressources = [];
