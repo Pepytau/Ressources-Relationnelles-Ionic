5da647f8809b9b1e777019bdff145fda3fe37756
Ezgralium <simon.provost@ymail.com>
2023-04-20 08:23:14 +0200

    Début changement image de profile

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Storage } from '@ionic/storage-angular';
@@ -19,14 +19,21 @@ export class ProfilePage implements OnInit {
     profile_picture: '',
   }
 
+
+
   constructor(private storage: Storage, private router: Router, private http: HttpClient,) { }
 
   async ngOnInit() {
     await this.storage.get('user').then((myUser) => {
       this.user = myUser;
-      alert(this.user.profile_picture);
+      let params = new HttpParams().set('path', this.user.profile_picture);
+      this.http.get("https://api.ezraspberry.com/api/v1/User/getProfilePicture", { params: params }).subscribe((response: any) => {
+        console.log(response);
+      });
+
     });
   }
+
 
   disconnect() {
     this.storage.clear();
